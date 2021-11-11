50dc0287a4f3a902a568801e0b9e161550e4d385
Gabriel Malaquias <5194287+GabrielMalakias@users.noreply.github.com>
2021-11-11 16:48:10 +0100

    Initial script

--- initial.rb
+++ initial.rb
@@ -2,32 +2,62 @@ TWO_MINUTES = 120
 THREE_MINUTES = 180
 
 class Person
+  STATES = %w(out_of_range offline online)
+
   def initialize(name:)
-    @name =
+    @name = name
+    @status = 'online'
   end
 
   def move_to(status)
     case status
-    when 'out_of_range'
-      puts "Moving #{self} to out_of_range"
-    when 'offline'
-      puts "Moving #{self} to offline"
+    when *STATES
+      puts "Moving #{self} to #{status}"
+      @status = status
     else
       puts "Invalid move"
     end
   end
 
-  def out_of_range?
+  def is?(status)
+    @status == status
+  end
+
+  def no_recent_location_received?
     location_received_at < Time.now - TWO_MINUTES
   end
 
   def location_received_at
-    Time.now - THREE_MINUTES if rand < 0.03
+    return Time.now - THREE_MINUTES if rand < 0.03
 
     Time.now
   end
 
   def to_s
-    "Person #{@name} last location received at #{@location_received_at}"
+    "#Person name:#{@name} status:#{@status}"
   end
+end
+
+POPULATION = (0...10).to_a.map { |i| Person.new(name: "Person ##{i}") }
+
+class RunCheck
+  def call
+    filter_online.each do |person|
+      puts person
+      person.move_to('offline') if person.is?('out_of_range')
+      person.move_to('out_of_range') if person.no_recent_location_received?
+    end
+  end
+
+  private
+
+  def filter_online
+    POPULATION.reject { |person| person.is?('offline') }
+  end
+end
+
+check = RunCheck.new
+while true
+  check.()
+  sleep(10)
 end
